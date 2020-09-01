8be404f72e64663481cf63468da11e91868e912a
Tursukov <Dmitry.Tursukov@x5.ru>
2020-09-01 07:17:21 +0500

    Make menu

---
+++ src/ru/x5/Main.java
@@ -0,0 +1,18 @@
+package ru.x5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите номер задачи которую требуется проверить:");
+        System.out.println("1. текст");
+        System.out.println("2. текст");
+        System.out.println("3. текст");
+        System.out.println("4. текст");
+        System.out.println("5. текст");
+        System.out.println("6. текст");
+        System.out.println("7. текст");
+        System.out.println("8. текст");
+        System.out.println("9. текст");
+
+    }
+}
