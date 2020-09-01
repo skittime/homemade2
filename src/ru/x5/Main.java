5ec7d4da6a448e6f5be39105d2d0c194e8e964de
skittime <skittime@gmail.com>
2020-09-01 08:00:00 +0500

    цикл выбора меню

--- src/ru/x5/Main.java
+++ src/ru/x5/Main.java
@@ -1,8 +1,11 @@
 package ru.x5;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Введите номер задачи которую требуется проверить:");
         System.out.println("1. текст");
         System.out.println("2. текст");
@@ -13,6 +16,19 @@ public class Main {
         System.out.println("7. текст");
         System.out.println("8. текст");
         System.out.println("9. текст");
+        int number = input.nextInt();
+
+        switch (number){
+            case 1:
+                Make1 result = new Make1();
+                System.out.println(result);
+                break;
+            default:
+                System.out.println("Выбор неверен");
+        }
+
+
+
 
     }
 }
