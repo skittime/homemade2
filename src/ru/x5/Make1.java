429ff45ddf5f4c54f7f3a150d44b539da9ab0b63
skittime <skittime@gmail.com>
2020-09-02 01:34:58 +0500

    Выполнена первая задача

--- src/ru/x5/Make1.java
+++ src/ru/x5/Make1.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Make1 {
     public Make1(){
         Scanner numbers = new Scanner(System.in);
-        System.out.println("Укажите количество вводимых элементов");
-        int size = numbers.nextInt();
-        System.out.println("Введите элементы:");
-        int nothing = numbers.nextInt();
-        int[] array = new int[size];
-        System.out.println(array.length -1);
+        System.out.println("Введите число");
+        double mass = numbers.nextDouble();
+        int size = (int) mass;
+        System.out.println(size%10);
+        return;
 
 
     }
