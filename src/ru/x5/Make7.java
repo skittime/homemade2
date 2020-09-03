c5f55bdf153715b4e5e53c297cf8e068ee0ce4f3
skittime <skittime@gmail.com>
2020-09-03 08:15:33 +0500

    Задача 7 работает

---
+++ src/ru/x5/Make7.java
@@ -0,0 +1,44 @@
+package ru.x5;
+
+import java.util.Scanner;
+
+public class Make7 {
+    public Make7 () {
+        System.out.println("Введите код города");
+        Scanner num = new Scanner(System.in);
+        int call = num.nextInt();
+
+            switch (call) {
+                case 905:
+                    double min;
+                    double a = 4.15;
+                    min = a * 10;
+                    System.out.println("Москва. Стоимость разговора " + min + " руб.");
+                    break;
+                case 194:
+                    a = 1.98;
+                    min = a * 10;
+                    System.out.println("Ростов. Стоимость разговора " + min + " руб.");
+                    break;
+                case 491:
+                    a = 2.69;
+                    min = a * 10;
+                    System.out.println("Краснодар. Стоимость разговора " + min + " руб.");
+                    break;
+                case 800:
+                    a = 5.00;
+                    min = a * 10;
+                    System.out.println("Киров. Стоимость разговора " + min + " руб.");
+                    break;
+                default:
+                    System.out.println("Введен неверный код");
+                    break;
+            }
+
+
+
+        }
+    }
+
+  
+
