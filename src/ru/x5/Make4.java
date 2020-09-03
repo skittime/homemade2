415d52d12b0c0bcbac40d0da8720a3a66946f4df
skittime <skittime@gmail.com>
2020-09-03 06:29:01 +0500

    Задача 4 работает

---
+++ src/ru/x5/Make4.java
@@ -0,0 +1,25 @@
+package ru.x5;
+
+import java.util.Scanner;
+
+public class Make4 {
+    public Make4() {
+        System.out.println("Введите число: ");
+        Scanner num = new Scanner(System.in);
+        int numbs = num.nextInt();
+
+        if (numbs > 0) {
+            numbs++;
+            System.out.println("Ответ " + numbs);
+        }
+        else if (numbs < 0) {
+            numbs = numbs - 2;
+            System.out.println("Ответ " + numbs);
+
+        }
+        else if (numbs == 0) {
+            numbs = 10;
+            System.out.println("Ответ " + numbs);
+        }
+    }
+}
