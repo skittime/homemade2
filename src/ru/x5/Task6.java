65b27efe9ef556cc6d8550fa11ad16155dbb10f9
skittime <skittime@gmail.com>
2020-09-08 03:14:38 +0500

    Правки 6-я задача

--- src/ru/x5/Task6.java
+++ src/ru/x5/Task6.java
@@ -3,31 +3,22 @@ package ru.x5;
 import java.util.Scanner;
 
 public class Task6 {
-    public void make6() {
+    public static void make6() {
         System.out.println("Введите число:");
-        Scanner num = new Scanner(System.in);
-        int abs = num.nextInt();
-        if (abs > 0) {
-            int pol;
-            pol = abs % 2;
-            if (pol == 0) {
-                System.out.println("Введено положительное четное число");
-            } else
-                System.out.println("Введено положительное нечетное число");
-
-        }
-
-        if (abs < 0) {
-            int pol;
-            pol = abs % 2;
-            if (pol == 0) {
-                System.out.println("Введено отрицательное четное число");
-            } else
-                System.out.println("Введено отрицательное нечетное число");
-
-        }
+        Scanner beIn = new Scanner(System.in);
+        int abs = beIn.nextInt();
+        int pol = abs % 2;
         if (abs == 0) {
             System.out.println("Введено нулевое число");
-        }
+        } else if (pol == 0) {
+            if (abs > 0) {
+                System.out.println("Введено положительное четное число");
+            } else
+                System.out.println("Введено отрицательное четное число");
+        } else if (abs > 0) {
+            System.out.println("Введено положительное нечетное число");
+        } else
+            System.out.println("Введено отрицательное нечетное число");
+
     }
 }
